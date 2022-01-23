69118db02f2c15b3d7c76a6bf08ad4221fe25e1a
Xithrius <xithrius@gmail.com>
2022-01-23 04:12:11 -0800

    Added Table struct with an as_markdown method

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
-pub fn hello_world() -> String {
-    "Hello, world!".to_string()
+#[derive(Debug)]
+pub struct Table {
+    boxes: Vec<Vec<String>>,
+}
+
+impl Table {
+    pub fn new(boxes: Vec<Vec<String>>) -> Self {
+        Table { boxes }
+    }
+
+    pub fn as_markdown(&self) -> String {
+        format!(
+            "<table>{}</table>",
+            self.boxes
+                .iter()
+                .map(|v| {
+                    format!(
+                        "<tr>{}",
+                        v.iter()
+                            .map(|v_inner| format!("<td>{}", v_inner))
+                            .collect::<Vec<String>>()
+                            .join("")
+                    )
+                })
+                .collect::<Vec<String>>()
+                .join("")
+        )
+    }
 }
