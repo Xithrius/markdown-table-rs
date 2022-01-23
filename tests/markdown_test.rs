b8d9c4d587b1f024d889502755f1c94c0a9abf55
Xithrius <xithrius@gmail.com>
2022-01-23 04:13:16 -0800

    Added simple Table to markdown test

---
+++ tests/markdown_test.rs
@@ -0,0 +1,19 @@
+#[cfg(test)]
+mod tests {
+    use markdown_table::*;
+
+    fn setup() -> Table {
+        Table::new(vec![
+            vec!["Testing".to_string()],
+            vec!["1".to_string()],
+            vec!["2".to_string()],
+        ])
+    }
+
+    #[test]
+    fn test_hello_world() {
+        let md: String = setup().as_markdown();
+
+        assert_eq!(md, "<table><tr><td>Testing<tr><td>1<tr><td>2</table>".to_string());
+    }
+}
--- tests/simple_test.rs
+++
@@ -1,9 +0,0 @@
-#[cfg(test)]
-mod tests {
-    use rust_library_project_template::hello_world;
-
-    #[test]
-    fn test_hello_world() {
-        assert_eq!(hello_world(), "Hello, world!".to_string());
-    }
-}
